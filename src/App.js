8cb1ee6b0f677862a2ef61c3c62d0d9294f6798c
kavdesilva <kavdesilva@gmail.com>
2022-07-13 23:56:58 -0400

    most of this was written based on hanging around office hours and modelling after the ATM exercise. comprehension is better, but i don't know if i could have done it on my own without the extra help.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import {useState} from 'react'
 import Validator from "./components/Validator"
 
 const App = () => {
 
+  let [username, setUsername] = useState('')
+  let [password, setPassword] = useState('')
+  let [passwordConfirm, setPasswordConfirm] = useState('')
+  let [valid, setValid] = useState(false)
+
+  const clickUsername = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const clickPassword = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const clickPasswordConfirm = (event) => {
+    setPasswordConfirm(event.target.value)
+  }
+
+  const clickValid = (event) => {
+    event.preventDefault()
+    if (password === passwordConfirm){
+      setValid(true)
+      console.log(valid)
+    } else {
+      setValid(false)
+      console.log(valid)
+    }
+  }
+
+  const clearForm = () => {
+    setUsername('')
+    setPassword('')
+    setPasswordConfirm('')
+  }
+
   return (
-    <Validator />
+    <Validator clickValid={clickValid}  
+               username={username} 
+               password={password} 
+               passwordConfirm={passwordConfirm}  
+               clickUsername={clickUsername} 
+               clickPassword={clickPassword} 
+               clickPasswordConfirm={clickPasswordConfirm}
+               clearForm={clearForm} />
   )
 }
 
--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -1,26 +1,42 @@
+import { useState } from 'react'
 import '../styles/Validator.css'
 
-const Validator = () => {
-  
+const Validator = (props) => {
+
   return (
     <div className="form">
       <h1>Sign Up</h1>
       <form>
-        <input type="text" placeholder="Username" id="username" />
+        <input 
+          type="text" 
+          placeholder="Username" 
+          id="username" 
+          onChange={props.clickUsername} 
+          value={props.username} 
+        />
         <label htmlFor="username">Username</label>
 
-        <input type="password" placeholder="Password" id="password" />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          id="password" 
+          onChange={props.clickPassword}
+          value={props.password} 
+        />
         <label htmlFor="password">Password</label>
 
         <input
           type="password"
           placeholder="Confirm password"
-          id="passwordConfirm"
+          id="passwordConfirm" 
+          onChange={props.clickPasswordConfirm}
+          value={props.passwordConfirm} 
         />
         <label htmlFor="passwordConfirm">Confirm password</label>
 
-        <button type="submit">Sign Up</button>
-        <p>Passwords must match.</p>
+        <button type="submit" onClick={props.clickValid}>Sign Up</button>
+        <button className="cancel" onClick={props.clearForm}>Clear Form</button>
+        <p className="password-message">Passwords must match.</p>
       </form>
     </div>
   )
